dff6befbac4a56d7745c1397fdfd0820fc50b4f8
personal <shinrainxie@gmail.com>
2014-12-29 12:05:50 -0800

    leetcode-python: add better solution for PermutationSequence.py

--- code/PermutationSequence.py
+++ code/PermutationSequence.py
@@ -1,37 +1,18 @@
 class Solution:
-	# @return a string
-	def getPermutation(self, n, k):
-		if n==0:
-			return ""
-		re=""
-		for i in range(n):
-			re+=str(i+1)
-		if k==1:
-		    return re
-		for i in range(1,k):
-			re=perm(re)
-		return re
 
-def perm(s):
-	s=list(s)
-	n=len(s)
-	i=n-1
-	while i>0 and str(s[i]) <=str(s[i-1]):
-		i-=1
-	if i==0:
-		s.sort()
-		return "".join(s)
-	j=n-1
-	while j>=i and s[j]<=s[i-1]:
-		j-=1
-	t=s[j]
-	s[j]=s[i-1]
-	s[i-1]=t
-	[l,r]=[i,n-1]
-	while l<r:
-		t=s[l]
-		s[l]=s[r]
-		s[r]=t
-		l+=1
-		r-=1
-	return "".join(s)
+	def getPermutation(self, n, k):
+		if n==0 or k==0:
+			return ""
+
+		num=[i for i in range(1,n+1)]
+		t=math.factorial(n-1)
+		k-=1
+		re=""
+		for i in range(n-1,0,-1):
+			p=k/t
+			re+=str(num[p])
+			num.remove(num[p])
+			k%=t
+			t=t/i
+		re+=str(num[0])
+		return re
